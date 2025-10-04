a911c3a5b2b4b2ebbe68b47d74c595b6e0c3aaea
Robert Nurnberg <robert.nurnberg@gmx.de>
2025-10-04 18:14:09 +0200

    fix

--- do_collect.sh
+++ do_collect.sh
@@ -15,12 +15,13 @@ python ../cdblib/cdbbulkpv.py -s -c 8 --stable --user rob DFRC_openings.epd.gz >
 
 $SCORE FRC_openings.epd.gz $TEMP_FILE >FRC_openings_cdbpv.epd
 mv "$TEMP_FILE" DFRC_openings_cdbpv.epd
-gzip -9 DFRC_openings_cdbpv.epd
 
 python ../caissatrack/caissatrack.py DFRC_openings_cdbpv.epd >>dfrctrack.csv
 python ../caissatrack/caissatrack.py FRC_openings_cdbpv.epd >>frctrack.csv
 python ../caissatrack/extract_fens.py DFRC_openings_cdbpv.epd --shortest 100 --ignore2folds >dfrc_weekly_shortest.epd
 python ../caissatrack/extract_fens.py DFRC_openings_cdbpv.epd --evalMin 90 --evalMax 105 >dfrc_weekly_edgy.epd
+
+gzip -9 DFRC_openings_cdbpv.epd
 
 git add DFRC_openings_cdbpv.epd.gz FRC_openings_cdbpv.epd dfrctrack.csv frctrack.csv dfrc_weekly_shortest.epd dfrc_weekly_edgy.epd
 git diff --staged --quiet || git commit -m "update data"
