fd38f9f138bb0ec1ce9c4d6def0fe9031f4837b2
Robert Nurnberg <robert.nurnberg@gmx.de>
2025-10-20 11:43:55 +0200

    frc edgy

--- do_update.sh
+++ do_update.sh
@@ -2,7 +2,7 @@
 
 python ../caissatrack/plotdata.py dfrctrack.csv --cutOff 300 --negplot -ll --edgeMin 90 --edgeMax 105
 
-python ../caissatrack/plotdata.py frctrack.csv --negplot -ll --edgeMin 90 --edgeMax 105
+python ../caissatrack/plotdata.py frctrack.csv --negplot -ll --edgeMin 80 --edgeMax 115
 
 git add dfrctrack.png dfrctrackpv.png dfrctracktime.png
 git add frctrack.png frctrackpv.png frctracktime.png
