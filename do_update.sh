751be2400a953c0127a6a98de76af4a860054ca3
Robert Nurnberg @ elitebook <robert.nurnberg@gmx.de>
2025-10-23 11:23:14 +0200

    Add tracker/plots for chess324

--- do_update.sh
+++ do_update.sh
@@ -4,7 +4,10 @@ python ../caissatrack/plotdata.py dfrctrack.csv --cutOff 300 --negplot -ll --edg
 
 python ../caissatrack/plotdata.py frctrack.csv --negplot -ll --edgeMin 80 --edgeMax 115
 
+python ../caissatrack/plotdata.py chess324track.csv --cutOff 150 --negplot -ll --edgeMin 90 --edgeMax 105
+
 git add dfrctrack.png dfrctrackpv.png dfrctracktime.png
 git add frctrack.png frctrackpv.png frctracktime.png
+git add chess324track.png chess324trackpv.png chess324tracktime.png
 git diff --staged --quiet || git commit -m "update plots"
 git push origin main >&push.log
